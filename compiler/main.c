0469964961cecee8cb654ea4e87e8f04e0f2f242
NakagamiYuta <guguru0014@gmail.com>
2019-09-17 16:08:59 +0900

    [remove] main関数定義をmain.cから削除した

--- compiler/main.c
+++ compiler/main.c
@@ -19,25 +19,11 @@ int main(int argc, char** argv){
     printf(".intel_syntax   noprefix\n");
     printf(".global         main\n");
     printf("\n");
-    printf("main:\n");
-
-    // ローカル変数領域確保
-    int lvar_cnt = 0;
-    for(LVar *tmp = locals; tmp->next != NULL; ++ lvar_cnt){ tmp = tmp->next; }
-    printf("        push rbp\n");
-    printf("        mov rbp, rsp\n");
-    printf("        add rsp, %d\n", 8*(lvar_cnt+20));
 
     // アセンブリ出力
     label = 0;
     for(int idx = 0; code[idx] != NULL; ++ idx){
         gen_asm(code[idx]);
-        printf("        pop rax\n");    // 式の最終的な値を取り出す
     }
-
-    // フッター
-    printf("        mov rsp, rbp\n");
-    printf("        pop rbp\n");
-    printf("        ret\n");
     return 0;
 }
