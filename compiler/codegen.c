25f73ed28380742be7ca726e06639917be4eb7ea
NakagamiYuta <guguru0014@gmail.com>
2019-09-21 15:49:33 +0900

    [clean] outasm関数作成

--- compiler/codegen.c
+++ compiler/codegen.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdarg.h>
 #include "yncc.h"
 
 
@@ -13,13 +14,22 @@ static char *regs32[] = {"r10d", "r11d", "ebx", "r12d", "r13d", "r14d", "r15d"};
 /* 引数用のレジスタ */
 static char *argregs[] = {"rdi", "rsi", "rdx", "rcx", "r8", "r9"};
 
+// アセンブリ出力
+void outasm(char *fmt, ...) {
+    printf("\t\t");
+    va_list va;
+    va_start(va, fmt);
+    vfprintf(stdout, fmt, va);
+    va_end(va);
+    printf("\n");
+}
 
 // 左辺値コンパイル
 void gen_lval(Node *node){
     if(node->kind == ND_LVER) {
-        printf("        mov rax, rbp\n");
-        printf("        sub rax, %d\n", node->offset);
-        printf("        push rax\n");
+        outasm("mov rax, rbp");
+        outasm("sub rax, %d", node->offset);
+        outasm("push rax");
         return;
     }
 
@@ -36,7 +46,7 @@ void gen_asm(Node *node);
 void gen_asm_with_pop(Node *node) {
     gen_asm(node);
     if(node != NULL) {
-        printf("        pop rax\n");
+        outasm("pop rax");
     }
 }
 
@@ -52,23 +62,23 @@ void gen_asm(Node *node){
     // 変数, 値, ブロック, 関数定義, 関数呼び出し, ポインタ
     switch(node->kind){
     case ND_NUM:
-        printf("        push %d\n", val);
+        outasm("push %d", val);
         return;
 
     case ND_LVER:   // 右辺に左辺値が出てきた場合
         gen_lval(node);
-        printf("        pop rax\n");
-        printf("        mov rax, [rax]\n");
-        printf("        push rax\n");
+        outasm("pop rax");
+        outasm("mov rax, [rax]");
+        outasm("push rax");
         return;
 
     case ND_ASSIGN:
-        gen_lval(left);                         // [a] = 9 + 1  : LEFT
-        gen_asm(right);                         // a = [9 + 1]  : RIGHT
-        printf("        pop rbx\n");            // RIGHT
-        printf("        pop rax\n");            // LEFT
-        printf("        mov [rax], rbx\n");     // [LEFT] = RIGHT
-        printf("        push rbx\n");           // a=b=c=8 が出来るように右辺値をスタックに残しておく
+        gen_lval(left);                 // [a] = 9 + 1  : LEFT
+        gen_asm(right);                 // a = [9 + 1]  : RIGHT
+        outasm("pop rbx");              // RIGHT
+        outasm("pop rax");              // LEFT
+        outasm("mov [rax], rbx");      // [LEFT] = RIGHT
+        outasm("push rbx");             // a=b=c=8 が出来るように右辺値をスタックに残しておく
         return;
 
     case ND_BLOCK:;
@@ -77,54 +87,54 @@ void gen_asm(Node *node){
             gen_asm_with_pop(block_node);
             block_node = block_node->block_next_node;
         }
-        printf("        push rax\n");
+        outasm("push rax");
         return;
 
     case ND_FUNC:{
         printf("\n");
         printf("%s:\n", node->f_name);
-        printf("        push rbx\n");
-        printf("        push rbp\n");
-        printf("        mov rbp, rsp\n");
-        printf("        push r12\n");
-        printf("        push r13\n");
-        printf("        push r14\n");
-        printf("        push r15\n");
-        printf("        sub rsp, %d\n", 8*20);
-        for(int idx = 0; idx < 6; ++ idx) {
-            if(node->args[idx]){
-                printf("        mov [rbp-%d], %s\n", idx*8+8, argregs[idx]);
-            }
-        }
+        outasm("push rbx");
+        outasm("push rbp");
+        outasm("mov rbp, rsp");
+        outasm("push r12");
+        outasm("push r13");
+        outasm("push r14");
+        outasm("push r15");
+        outasm("sub rsp, %d", 8*20);
+
+        for(int idx = 0; idx < 6 && node->args[idx]; ++ idx)
+            outasm("mov [rbp-%d], %s", idx*8+8, argregs[idx]);
         gen_asm_with_pop(left);
-        printf("        mov rax, 0\n");
-        printf("        pop r15\n");
-        printf("        pop r14\n");
-        printf("        pop r13\n");
-        printf("        pop r12\n");
-        printf("        mov rsp, rbp\n");
-        printf("        pop rbp\n");
-        printf("        pop rbx\n");
-        printf("        ret\n\n");
+
+        outasm("mov rax, 0");
+        outasm("pop r15");
+        outasm("pop r14");
+        outasm("pop r13");
+        outasm("pop r12");
+        outasm("mov rsp, rbp");
+        outasm("pop rbp");
+        outasm("pop rbx");
+        outasm("ret");
+        printf("\n");
         return;
     }
 
     case ND_CALL_FUNC:{
-        printf("        push rdi\n");                                       // rdi, rsi
-        printf("        push rsi\n");
+        outasm("push rdi");                                       // rdi, rsi
+        outasm("push rsi");
         for(int idx = 0; idx < 6; ++ idx) {
             if(node->args[idx]) {
                 gen_asm_with_pop(node->args[idx]);
-                printf("        mov %s, rax\n", argregs[idx]);
+                outasm("mov %s, rax", argregs[idx]);
             }
         }
-        printf("        mov rbx, rsp\n");
-        printf("        and rsp, 0xfffffffffffffff0\n");                    // アライメント
-        printf("        call %s\n", node->f_name);
-        printf("        mov rsp, rbx\n");
-        printf("        pop rsi\n");
-        printf("        pop rdi\n");
-        printf("        push rax\n");
+        outasm("mov rbx, rsp");
+        outasm("and rsp, 0xfffffffffffffff0");                    // アライメント
+        outasm("call %s", node->f_name);
+        outasm("mov rsp, rbx");
+        outasm("pop rsi");
+        outasm("pop rdi");
+        outasm("push rax");
         return;
     }
 
@@ -134,9 +144,9 @@ void gen_asm(Node *node){
 
     case ND_DEREF:
         gen_asm(left);
-        printf("        pop rax\n");
-        printf("        mov rax, [rax]\n");
-        printf("        push rax\n");
+        outasm("pop rax");
+        outasm("mov rax, [rax]");
+        outasm("push rax");
         return;
     }
 
@@ -144,57 +154,66 @@ void gen_asm(Node *node){
     switch(node->kind){
     case ND_RETURN:
         gen_asm_with_pop(left);
-        printf("        mov rsp, rbp\n");
-        printf("        pop rbp\n");
-        printf("        pop rbx\n");
-        printf("        ret\n");
+        outasm("mov rsp, rbp");
+        outasm("pop rbp");
+        outasm("pop rbx");
+        outasm("ret");
         return;
 
     case ND_IF:
         label ++;
+        // 条件式
         gen_asm_with_pop(left);
-        printf("        cmp rax, 1\n");
-        printf("        jne .L__if_else_%d\n", tmp_label);
+        outasm("cmp rax, 1");
+        outasm("jne .L__if_else_%d", tmp_label);
+        // 処理<true>
         gen_asm_with_pop(right->left);
-        printf("        jmp .L__if_end_%d\n", tmp_label);
+        outasm("jmp .L__if_end_%d", tmp_label);
+        // 処理<false>
         printf(".L__if_else_%d:\n", tmp_label);
         gen_asm_with_pop(right->right);
         printf(".L__if_end_%d:\n", tmp_label);
-        printf("        push 0\n");
+        outasm("push 0");
         return;
 
     case ND_WHILE:
         label ++;
         printf(".L__while_start_%d:\n", tmp_label);
+        // 条件式
         gen_asm_with_pop(left);
-        printf("        cmp rax, 1\n");
-        printf("        jne .L__while_end_%d\n", tmp_label);
+        outasm("cmp rax, 1");
+        outasm("jne .L__while_end_%d", tmp_label);
+        // 処理
         gen_asm_with_pop(right);
-        printf("        jmp .L__while_start_%d\n", tmp_label);
+        outasm("jmp .L__while_start_%d", tmp_label);
         printf(".L__while_end_%d:\n", tmp_label);
-        printf("        push 0\n");
+        outasm("push 0");
         return;
 
     case ND_FOR:
         label ++;
+        // 初期化
         gen_asm_with_pop(left);
         printf(".L__for_start_%d:\n", tmp_label);
+        // 条件式
         gen_asm_with_pop(right->left->left);
-        printf("        cmp rax, 1\n");
-        printf("        jne .L__for_end_%d\n", tmp_label);
+        outasm("cmp rax, 1");
+        outasm("jne .L__for_end_%d", tmp_label);
+        // 処理
         gen_asm_with_pop(right->left->right);
+        // 変化式
         gen_asm_with_pop(right->right);
-        printf("        jmp .L__for_start_%d\n", tmp_label);
+        outasm("jmp .L__for_start_%d", tmp_label);
         printf(".L__for_end_%d:\n", tmp_label);
-        printf("        push 0\n");
+        outasm("push 0");
         return;
     }
 
     gen_asm(left);
     gen_asm(right);
 
-    printf("        pop rbx\n");
-    printf("        pop rax\n");
+    outasm("pop rbx");
+    outasm("pop rax");
 
     bool is_left_ptr = (left->type != NULL && left->type->ty == PTR);
     bool is_right_ptr = (right->type != NULL && right->type->ty == PTR);
@@ -203,62 +222,62 @@ void gen_asm(Node *node){
     switch(node->kind){
     case ND_ADD:
         if(is_left_ptr)
-            printf("        imul rbx, %d\n", type_to_size(left->type->ptr_to));
+            outasm("imul rbx, %d", type_to_size(left->type->ptr_to));
         if(is_right_ptr)
-            printf("        imul rax, %d\n", type_to_size(right->type->ptr_to));
-        printf("        add rax, rbx\n");
+            outasm("imul rax, %d", type_to_size(right->type->ptr_to));
+        outasm("add rax, rbx");
         break;
 
     case ND_SUB:
         if(is_left_ptr)
-            printf("        imul rbx, %d\n", type_to_size(left->type->ptr_to));
+            outasm("imul rbx, %d", type_to_size(left->type->ptr_to));
         if(is_right_ptr)
-            printf("        imul rax, %d\n", type_to_size(right->type->ptr_to));
-        printf("        sub rax, rbx\n");
+            outasm("imul rax, %d", type_to_size(right->type->ptr_to));
+        outasm("sub rax, rbx");
         break;
 
     case ND_MUL:
-        printf("        imul rax, rbx\n");
+        outasm("imul rax, rbx");
         break;
 
     case ND_DIV:
-        printf("        cqo\n");        // raxレジスタをrdxと合わせた128bitに拡張
-        printf("        idiv rbx\n");   // rax / rsiの結果 (余りはrdx)
+        outasm("cqo");        // raxレジスタをrdxと合わせた128bitに拡張
+        outasm("idiv rbx");   // rax / rsiの結果 (余りはrdx)
         break;
 
     case ND_DIV_REMAIN:
-        printf("        cqo\n");
-        printf("        idiv rbx\n");
-        printf("        mov rax, rdx\n");
+        outasm("cqo");
+        outasm("idiv rbx");
+        outasm("mov rax, rdx");
         break;
 
     case ND_EQ:
-        printf("        cmp rbx, rax\n");   // rdiとraxを比較 -> 結果はフラグレジスタへ
-        printf("        sete al\n");        // 比較結果(==)をalに入れる(raxの下位8ビットにあたるレジスタ)
-        printf("        movzb rax, al\n");  // raxレジスタの上位56ビットをゼロクリア
+        outasm("cmp rbx, rax");   // rdiとraxを比較 -> 結果はフラグレジスタへ
+        outasm("sete al");        // 比較結果(==)をalに入れる(raxの下位8ビットにあたるレジスタ)
+        outasm("movzb rax, al");  // raxレジスタの上位56ビットをゼロクリア
         break;
 
     case ND_NEQ:
-        printf("        cmp rbx, rax\n");
-        printf("        setne al\n");       // 比較結果(!=)をalに入れる
-        printf("        movzb rax, al\n");
+        outasm("cmp rbx, rax");
+        outasm("setne al");       // 比較結果(!=)をalに入れる
+        outasm("movzb rax, al");
         break;
 
     case ND_UPPERL:
-        printf("        cmp rbx, rax\n");
-        printf("        setl al\n");        // 比較結果(>)をalに入れる
-        printf("        movzb rax, al\n");
+        outasm("cmp rbx, rax");
+        outasm("setl al");        // 比較結果(>)をalに入れる
+        outasm("movzb rax, al");
         break;
 
     case ND_UPPEREQL:
-        printf("        cmp rbx, rax\n");
-        printf("        setle al\n");       // 比較結果(>=)をalに入れる
-        printf("        movzb rax, al\n");
+        outasm("cmp rbx, rax");
+        outasm("setle al");       // 比較結果(>=)をalに入れる
+        outasm("movzb rax, al");
         break;
 
     default:
         error("[ERROR] 構文木解析エラー");
     }
 
-    printf("        push rax\n");
+    outasm("push rax");
 }
