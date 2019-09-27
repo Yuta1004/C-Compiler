84eb9c0713853bb0b3770801ac6645d8548d824b
NakagamiYuta <guguru0014@gmail.com>
2019-09-27 16:03:05 +0900

    [add] vector.c & vector.h

---
+++ compiler/vector.h
@@ -0,0 +1,18 @@
+#ifndef MY_VECTOR_H
+#define MY_VECTOR_H
+
+typedef struct Vector Vector;
+struct Vector{
+    void **data;
+    int capacity;
+    int len;
+};
+
+Vector *vec_new(int capacity);
+void *vec_push(Vector *vec, void *item);
+void *vec_pop(Vector *vec);
+void *vec_get(Vector *vec, int idx);
+void *vec_add_capacity(Vector *vec, int size);
+void vec_free(Vector *free);
+
+#endif // !MY_VECTOR_H
