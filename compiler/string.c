23c4c4b92fe824eef75c1b06d0931e388002c8b6
NakagamiYuta <guguru0014@gmail.com>
2019-09-27 21:57:16 +0900

    [fix] find_str内の処理に誤りがあったので修正

--- compiler/string.c
+++ compiler/string.c
@@ -4,7 +4,7 @@
 int find_str(char *target_str) {
     for(int idx = 0; idx < str_vec->len; ++ idx) {
         char *str = (char*)vec_get(str_vec, idx);
-        if(strcmp(target_str, str) == 0) {
+        if(strncmp(target_str, str, strlen(str)) == 0) {
             return idx;
         }
     }
