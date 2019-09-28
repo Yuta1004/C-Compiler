85179f60ebbe8adcf997760e1e83c963360638d3
NakagamiYuta <guguru0014@gmail.com>
2019-09-28 16:31:37 +0900

    [change] 関数呼び出しの引数処理部分の実装を変更した

--- compiler/parse.c
+++ compiler/parse.c
@@ -384,19 +384,9 @@ Node *primary(){
             // 引数
             node->args = calloc(6, sizeof(Node));
             for(int idx = 0; idx < 6; ++ idx) {
-                Token *bef_token = token;
-                while(  consume("*") ||
-                        consume("&") ||
-                        consume("(") ||
-                        (consume_kind(TOKEN_SIZEOF) && consume("("))
-                );
-                if(consume_number() || consume_ident() || consume_kind(TOKEN_STR)){
-                    token = bef_token;
-                    node->args[idx] = expr();
-                }
-                if(!consume(",")){
-                    break;
-                }
+                if(strncmp(token->str, ")", 1) == 0) break;
+                node->args[idx] = expr();
+                if(!consume(",")) break;
             }
             expect(")");
             return node;
