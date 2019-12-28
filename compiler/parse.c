c1cf52f940ad0a50990ecd7096666b9f9f51a6f7
NakagamiYuta <guguru0014@gmail.com>
2019-12-28 13:24:52 +0900

    [add] 複合代入(+=, -=, *=, /=)実装

--- compiler/parse.c
+++ compiler/parse.c
@@ -288,6 +288,14 @@ Node *assign(){
     Node *node = equality();
     if(consume("=")){
         node = new_node(ND_ASSIGN, node, assign());
+    } else if(consume("+=")){
+        node = new_node(ND_ASSIGN, node, new_node(ND_ADD, node, assign()));
+    } else if(consume("-=")){
+        node = new_node(ND_ASSIGN, node, new_node(ND_SUB, node, assign()));
+    } else if(consume("*=")){
+        node = new_node(ND_ASSIGN, node, new_node(ND_MUL, node, assign()));
+    } else if(consume("/=")){
+        node = new_node(ND_ASSIGN, node, new_node(ND_DIV, node, assign()));
     } else {
         return node;
     }
--- compiler/test.sh
+++ compiler/test.sh
@@ -190,5 +190,13 @@ try "int main() { int sum = 0; for(int a = 1; a <= 10; a = a+1) sum = sum+a; ret
 try "int main() { int sum = 0; for(int i = 1; i <= 5; i = i+1) for(int j = 1; j <= 5; j = j+1) sum = sum+j; return sum; }" 75
 try "int main() { int sum = 0; for(int i = 1; i <= 10; i = i+1) { for(int j = 1; j <= 10; j = j+1) { int sum = 1204 + j; } sum = sum+i; } return sum; }" 55
 
+## comp-assign
+echo -e "\e[1m\ncomp-assign\e[m"
+try "int main() { int a = 0; a += 10; a -= 5; return a; }" 5
+try "int main() { int a = 10; a *= 5; a /= 10; return a; }" 5
+try "int main() { int sum = 0; for(int n = 1; n <= 10; n += 1) { sum += n; } return sum; }" 55
+
+
+
 echo ""
 exit 0
