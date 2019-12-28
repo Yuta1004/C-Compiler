444363ef2ba4439309daf1556383424b487f7859
NakagamiYuta <guguru0014@gmail.com>
2019-12-28 14:26:00 +0900

    [add] ++, --をトークナイズできるようにした

--- compiler/tokenize.c
+++ compiler/tokenize.c
@@ -45,7 +45,8 @@ Token *tokenize(char *p){
         if(strncmp(p, "<=", 2) == 0 || strncmp(p, ">=", 2) == 0 ||
            strncmp(p, "==", 2) == 0 || strncmp(p, "!=", 2) == 0 ||
            strncmp(p, "+=", 2) == 0 || strncmp(p, "-=", 2) == 0 ||
-           strncmp(p, "*=", 2) == 0 || strncmp(p, "/=", 2) == 0) {
+           strncmp(p, "*=", 2) == 0 || strncmp(p, "/=", 2) == 0 ||
+           strncmp(p, "++", 2) == 0 || strncmp(p, "--", 2) == 0) {
             cur = new_token(TOKEN_RESERVED, cur, p);
             cur->len = 2;
             p += 2;
