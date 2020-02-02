57d4828d03ae1968c6bf4e965144aea76a3af123
NakagamiYuta <guguru0014@gmail.com>
2020-02-02 16:24:15 +0900

    [add] parse.cを分割するためのディレクトリ、プログラムファイル群を生成

---
+++ compiler/parse/parse.h
@@ -0,0 +1,26 @@
+#ifndef PARSE_H
+#define PARSE_H
+
+
+/* 上位 (優先度:低) */
+Node *func();
+Node *block();
+Node *stmt();
+Node *expr();
+Node *assign();
+Node *equality();
+Node *log_or();
+Node *log_and();
+Node *bit_or();
+Node *bit_xor();
+Node *bit_and();
+Node *relational();
+Node *bit_shift();
+Node *add();
+Node *mul();
+Node *unary();
+Node *accessor();
+Node *primary();
+/* 下位 (優先度:高) */
+
+#endif 
