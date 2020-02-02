129f4121eea8b97daebdd36c575bb20c92e92d6e
NakagamiYuta <guguru0014@gmail.com>
2020-02-02 16:36:50 +0900

    [add] parse/block.c

---
+++ compiler/parse/block.c
@@ -0,0 +1,22 @@
+#include "../yncc.h"
+#include "../vector.h"
+#include "parse.h"
+
+// block = "{" block* "}" | stmt
+Node *block() {
+    // Block
+    if(consume("{")) {
+        Node *node = new_node(ND_BLOCK);
+        node->node_list = vec_new(10);
+
+        // stmt*
+        in_scope();
+        Node *now_node = node;
+        while(!consume("}")) {
+            vec_push(node->node_list, block());
+        }
+        out_scope();
+        return node;
+    }
+    return stmt();
+}
